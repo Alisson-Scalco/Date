ce552caab86e5708b2b6cd6fd7904a6648040321
Alisson-Scalco <107498874+Alisson-Scalco@users.noreply.github.com>
2022-06-16 17:05:19 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 getDateHR = () => {
     const hora = document.getElementById('hora')
     const data = new Date()
-    const teste = {weekday: 'long', year: 'numeric', month: 'long', day:
+    const opcao = {weekday: 'long', year: 'numeric', month: 'long', day:
     'numeric', hour: 'numeric', minute: 'numeric', second:'numeric' }
-    hora.innerHTML = data.toLocaleDateString('pt-BR', teste)
+    hora.innerHTML = data.toLocaleDateString('pt-BR', opcao)
     return hora
 }
 getDateHR()
